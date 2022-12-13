6d07e22c74098d73c672b0eeeff3560fb84ed7b1
williv17 <51355856+williv17@users.noreply.github.com>
2022-12-13 14:08:00 -0800

    Added my name

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 /************************************************************** 
 * Class:  CSC-615-01 Fall 2022 
-* Name: Tony Huang, Jess Hollingsworth, Jesse Henrick
+* Name: Tony Huang, Jess Hollingsworth, Jesse Henrick, Willi Vargas
 * Student ID: 915090688, 920974506, 921323452
 * Github ID: tzphuang, jesshollingsworth, jessehenrick
 * Project: Group Term Project
